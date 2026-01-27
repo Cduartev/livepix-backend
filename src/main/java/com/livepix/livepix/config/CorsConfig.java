bb98541b3e136344239bb5797375b8d400c795f3
Cduartev <cayooduartevidal@gmail.com>
2026-01-26 22:03:21 -0300

    feat- Ajuste de cors para libear a porta no front-end

---
+++ src/main/java/com/livepix/livepix/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.livepix.livepix.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000"
+                        // quando usar ngrok no front, adicione aqui também:
+                        // , "https://SEU-FRONT.ngrok-free.dev"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
