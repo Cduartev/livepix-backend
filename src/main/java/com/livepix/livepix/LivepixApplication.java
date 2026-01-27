afc26bc98c6051def4faf09f8cae1d69cdc7eb51
Cduartev <cayooduartevidal@gmail.com>
2026-01-26 22:22:58 -0300

    chore: inicializa estrutura do projeto Spring Boot

---
+++ src/main/java/com/livepix/livepix/LivepixApplication.java
@@ -0,0 +1,13 @@
+package com.livepix.livepix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivepixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivepixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livepix/livepix/LivepixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livepix.livepix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivepixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
